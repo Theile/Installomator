469ee7328030fb924485354463277b706dd8603f
Theile <soren@theilgaard.dk>
2024-04-04 08:52:19 +0200

    Update mactex.sh

--- fragments/labels/mactex.sh
+++ fragments/labels/mactex.sh
@@ -1,6 +1,6 @@
 mactex)
     name="MacTeX"
-    desription='Big 5 GB LaTeX distribution. Small alternative is `basictex`
+    description='Big 5 GB LaTeX distribution. Small alternative is `basictex`
 Includes these apps (maybe in older version): `texshop` (`texshop-alternative`), `texliveutility`, `latexit`, `bibdesk`.
 Could be a good idea to run those labels after this one to update apps fully.'
     appName="TeX Live Utility.app"
